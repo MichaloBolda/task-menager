dea9717c102df53f94c7e0e52c967977bb56fe1f
Michal Bolda <michalobold00@gmail.com>
2026-01-22 13:33:31 +0100

    Add Project and Task models, register in admin, initail migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects.models import Project  # upewnij się, że Project istnieje
 
-# Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('todo', 'To do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='todo'
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tasks'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
