09bde160d6947f7675b3a679c2dc17d35c812f34
Michal Bolda <michalobold00@gmail.com>
2026-01-22 23:37:14 +0100

    Add DRF API for projects and tasks

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects',
     'tasks',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from projects.views import ProjectViewSet
+from tasks.views import TaskViewSet
+from django.contrib import admin
 from django.urls import path
+
+router = routers.DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
